182acf4d59b801c4480da78af2a0128915d7448f
Filip Matynia <koncepcjonista37@@gmail.com>
2022-05-25 21:38:00 +0200

    feat(sidebar): Create sidebar component (#1)

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="bar">
+    <b-field>
+      <b-button expanded v-model="tmp" type="is-danger" @click="home">
+        Home
+      </b-button>
+    </b-field>
+    <b-field>
+      <b-button expanded v-model="tmp" type="is-warning" @click="about">
+        About
+      </b-button>
+    </b-field>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class Sidebar extends Vue {
+  @Prop() private msg!: string;
+
+  home() {
+    this.$router.push(`/`);
+  }
+
+  about() {
+    this.$router.push(`/about`);
+  }
+}
+</script>
+
+<style scoped>
+.bar {
+  display: flex;
+  padding: .5rem;
+  background-color: #1e1e1e;
+  width: 20rem;
+  height: 100vh;
+}
+</style>
