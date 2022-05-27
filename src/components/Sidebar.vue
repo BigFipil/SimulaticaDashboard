fe6138dab8bdc62e2884c4da92a6caae39d182fc
Filip Matynia <koncepcjonista37@@gmail.com>
2022-05-27 18:19:17 +0200

    feat(sidebar): Create basic options in sidebar

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,15 +1,55 @@
 <template>
-  <div class="bar">
-    <b-field>
-      <b-button expanded v-model="tmp" type="is-danger" @click="home">
-        Home
-      </b-button>
-    </b-field>
-    <b-field>
-      <b-button expanded v-model="tmp" type="is-warning" @click="about">
-        About
-      </b-button>
-    </b-field>
+  <div :class="[isExpanded ? `expanded` : '', `bar`]">
+    <div class="is_one_third">
+      <div class="bar__header">
+        <b-button type="is-primary" @click="toggle">
+          <b-icon icon="menu" size="is-small" />
+        </b-button>
+      </div>
+
+      <div v-if="isExpanded" class="bar__logo">
+        <img alt="Simulatica logo" src="../assets/logo.svg" />
+      </div>
+    </div>
+
+    <div class="bar__menu is_one_third">
+      <b-field>
+        <b-button :expanded="isExpanded" type="is-primary" icon-left="wrench" @click="home">
+          <div v-if="isExpanded" class="nav_button">
+            Main config
+          </div>
+        </b-button>
+      </b-field>
+      <b-field>
+        <b-button :expanded="isExpanded" type="is-primary" icon-left="atom-variant" @click="home">
+          <div v-if="isExpanded" class="nav_button">
+            Particles
+          </div>
+        </b-button>
+      </b-field>
+      <b-field>
+        <b-button :expanded="isExpanded" type="is-primary" icon-left="resistor-nodes" @click="home">
+          <div v-if="isExpanded" class="nav_button">
+            Node editor
+          </div>
+        </b-button>
+      </b-field>
+      <b-field>
+        <b-button :expanded="isExpanded" type="is-primary" icon-left="list-status" @click="home">
+          <div v-if="isExpanded" class="nav_button">
+            Examples
+          </div>
+        </b-button>
+      </b-field>
+      <b-field>
+        <b-button :expanded="isExpanded" type="is-primary"  icon-left="information" @click="home">
+          <div v-if="isExpanded" class="nav_button">
+            About
+          </div>
+        </b-button>
+      </b-field>
+    </div>
+
   </div>
 </template>
 
@@ -19,6 +59,12 @@ import { Component, Prop, Vue } from "vue-property-decorator";
 @Component
 export default class Sidebar extends Vue {
   @Prop() private msg!: string;
+
+  isExpanded = true;
+
+  toggle() {
+    this.isExpanded = !this.isExpanded;
+  }
 
   home() {
     this.$router.push(`/`);
@@ -33,9 +79,39 @@ export default class Sidebar extends Vue {
 <style scoped>
 .bar {
   display: flex;
-  padding: .5rem;
+  flex-direction: column;
+  padding: 1rem;
   background-color: #1e1e1e;
-  width: 20rem;
   height: 100vh;
+}
+
+.nav_button {
+  text-align: left;
+  width: 9rem;
+  margin-left: .5rem;
+}
+
+.is_one_third {
+  height: 33%;
+}
+
+.expanded {
+  width: 20rem;
+}
+
+.bar__header {
+  display: flex;
+  width: 100%;
+  justify-content: end;
+}
+
+.bar__logo {
+  margin-top: 1.5rem;
+}
+
+.bar__menu {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
 }
 </style>
