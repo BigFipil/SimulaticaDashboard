fb5b0afce1813770a69bcbd3c3d63c2b2dec913c
Filip Matynia <koncepcjonista37@@gmail.com>
2022-06-02 19:32:10 +0200

    feat(components): Create form component

---
+++ src/components/Form.vue
@@ -0,0 +1,33 @@
+<template lang="pug">
+  .form
+    .label
+      b {{ label }}
+    .content
+      slot
+
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class Sidebar extends Vue {
+  @Prop() private label!: string;
+
+}
+</script>
+
+<style scoped>
+.form {
+  display: flex;
+  flex-direction: column;
+  margin: 1rem;
+}
+.label {
+  font-size: 2rem;
+}
+.content {
+  padding-left: 1rem;
+  margin-bottom: 3rem;
+}
+</style>
