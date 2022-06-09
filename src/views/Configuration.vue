4af5ea59bdff22673aab925ad706a0e92aa64f31
Filip Matynia <koncepcjonista37@@gmail.com>
2022-06-09 14:48:12 +0200

    fix(configuration): Make configuration view resactive and binded to store

--- src/views/Configuration.vue
+++ src/views/Configuration.vue
@@ -5,46 +5,47 @@
         Form(label="Simulation settings")
           div
             b-field(label='Iteration count:')
-              b-input(v-model.number='settings.IterationCount' type='number')
+              b-input(v-model.number='localSettings.IterationCount' type='number' @input="update")
 
             b-field(label='Step time:')
-              b-input(v-model.number='settings.SimulationStepTime' type='number')
+              b-input(v-model.number='localSettings.SimulationStepTime' type='number' @input="update")
 
             b-field(label='Data savings step time:')
-              b-input(v-model.number='settings.DataSaveStepTime' type='number')
+              b-input(v-model.number='localSettings.DataSaveStepTime' type='number' @input="update")
 
             b-field(label='Memory mode:')
-              b-switch(v-model='settings.FullRamMode' disabled='true')
-                | {{ fullRamMode ? `Tylko pamięć ram` : `Bez ograniczeń` }}
+              b-switch(v-model='localSettings.FullRamMode' disabled='true' @input="update")
+                | {{ localSettings.FullRamMode ? `Tylko pamięć ram` : `Bez ograniczeń` }}
 
       .column
         Form(label="Output settings")
           div
             b-field(label='Simulation type:')
-              b-select(v-model='simulationType' expanded='')
+              b-select(v-model='localSettings.SimulationType' expanded='' @input="update")
                 option(v-for='type in simulationTypes' :key='type' :value='type' v-text='type')
 
             .columns.no__margin
               b-field.column.no__margin(label='X' expanded='')
-                b-input(v-model.number='simulationBoxSize.X' type='number')
+                b-input(v-model.number='localSettings.SimulationBoxSize.X' type='number' @input="update")
 
               b-field.column.no__margin(label='Y')
-                b-input(v-model.number='simulationBoxSize.Y' type='number')
+                b-input(v-model.number='localSettings.SimulationBoxSize.Y' type='number' @input="update")
 
               b-field.column.no__margin(v-if='simulationType == `3D`' label='Z')
-                b-input(v-model.number='simulationBoxSize.Z' type='number')
+                b-input(v-model.number='localSettings.SimulationBoxSize.Z' type='number' @input="update")
 
             b-field(label='Animation framerate:')
-              b-input(v-model.number='settings.OutputAnimationFramerate' type='number')
+              b-input(v-model.number='localSettings.OutputAnimationFramerate' type='number' @input="update")
 
             b-field(label='Output path:')
-              b-input(v-model='settings.OutputPath')
+              b-input(v-model='localSettings.OutputPath' @input="update")
 
 </template>
 
 <script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
+import { Component, Prop, Vue, Watch } from "vue-property-decorator";
 import Form from "@/components/Form.vue";
+import { Settings } from "@/types";
 
 @Component({
   components: {
@@ -52,25 +53,26 @@ import Form from "@/components/Form.vue";
   },
 })
 export default class Configuration extends Vue {
-  settings = {
-    IterationCount: 100,
-    SimulationStepTime: 1,
-    DataSaveStepTime: 10,
-    FullRamMode: true,
-    OutputPath: ``,
-    OutputAnimationFramerate: 12,
-    Threads: 1,
-  };
-
-  simulationBoxSize = {
-    X: 10,
-    Y: 10,
-    Z: 10,
-  };
-
-  simulationType = `3D`;
-
   simulationTypes = [`2D`, `3D`];
+
+  localSettings: Settings = this.settings;
+
+  @Watch(`settings`)
+  onSettingsChange(value: Settings) {
+    this.localSettings = { ...value };
+  }
+
+  update() {
+    this.$store.commit(`updateSettings`, this.localSettings);
+  }
+
+  get settings() {
+    return this.$store.state.settings;
+  }
+
+  get simulationType() {
+    return this.localSettings.SimulationType;
+  }
 }
 </script>
 
