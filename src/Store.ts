dbee7b06b70ef93b26c936916ca79df6dabf1650
Filip Matynia <koncepcjonista37@@gmail.com>
2022-06-16 14:07:49 +0200

    feat(store): Add particles

--- src/Store.ts
+++ src/Store.ts
@@ -1,16 +1,21 @@
 import Vue from "vue";
 import Vuex from "vuex";
-import { Settings, emptySettings } from "@/types";
+import { Settings, emptySettings, Particle } from "@/types";
 
 Vue.use(Vuex);
 
 export const store = new Vuex.Store({
   state: {
     settings: emptySettings(),
+    particles: <Particle[]>[],
   },
   mutations: {
     updateSettings(state, newSettings: Settings) {
       state.settings = newSettings;
+    },
+
+    updateParticles(state, newParticles: Particle[]) {
+      state.particles = newParticles;
     },
   },
 });
