2387bdbe2daf288f3c91de221b4f84ec9b3cd4b2
Filip Matynia <koncepcjonista37@@gmail.com>
2022-07-13 16:55:34 +0200

    feat(main): ✨ Register baklava as vue component

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,12 @@ import 'buefy/dist/buefy.css';
 import '@/assets/style.css';
 import Vuex from 'vuex'
 import { store } from '@/Store'
+import { BaklavaVuePlugin } from '@baklavajs/plugin-renderer-vue'
+import '@baklavajs/plugin-renderer-vue/dist/styles.css'
 
 Vue.config.productionTip = false;
 Vue.use(Buefy);
+Vue.use(BaklavaVuePlugin);
 
 new Vue({
   store,
