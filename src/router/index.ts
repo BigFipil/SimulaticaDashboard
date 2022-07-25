42c369a2a1ba387dd2286296eaa3bb75f9d0943a
Filip Matynia <koncepcjonista37@@gmail.com>
2022-07-25 12:40:49 +0200

    ✨ feat(router): Make about page the default one

--- src/router/index.ts
+++ src/router/index.ts
@@ -8,7 +8,7 @@ Vue.use(VueRouter);
 
 const routes: Array<RouteConfig> = [
   {
-    path: "/about",
+    path: "/",
     name: "About",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
