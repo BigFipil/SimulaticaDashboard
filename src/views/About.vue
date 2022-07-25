b80dd2df128504c89a4d97765b0360a50a96b5e9
Filip Matynia <koncepcjonista37@@gmail.com>
2022-07-25 13:05:29 +0200

    🔨 refactor(about-page): Add empty content

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,30 +1,24 @@
-<template>
-  <div class="about">
-    <!-- <img alt="Vue logo" src="../assets/logo.png" /> -->
-    <!-- <Sidebar/> -->
-    <b-field>
-      <b-button expanded v-model="tmp" type="is-danger">
-        Button 1
-      </b-button>
-    </b-field>
-    <b-field>
-      <b-button expanded v-model="tmp" type="is-danger">
-        Button 2
-      </b-button>
-    </b-field>
-    <b-field>
-      <b-button expanded v-model="tmp" type="is-danger">
-        Button 3
-      </b-button>
-    </b-field>
-  </div>
+<template lang="pug">
+.content
+  h1.heading Coming soon
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 
 @Component
-export default class About extends Vue {
-  tmp2 = null;
-}
+export default class About extends Vue {}
 </script>
+
+<style lang="scss">
+.content {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+}
+
+.heading {
+  font-size: 3rem;
+}
+</style>
