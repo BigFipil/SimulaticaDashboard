0f33e52fd9b8c17404cbaed83ddcce6d76b1821b
Filip Matynia <koncepcjonista37@@gmail.com>
2022-07-25 13:12:55 +0200

    ✨ feat(examples): Create empty examples page

---
+++ src/views/Examples.vue
@@ -0,0 +1,24 @@
+<template lang="pug">
+.content
+  h1.heading Coming soon
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class Examples extends Vue {}
+</script>
+
+<style lang="scss">
+.content {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+}
+
+.heading {
+  font-size: 3rem;
+}
+</style>
